6b6777ef2f27f7d1dbc901e218580ed0ad7c1725
Koen <koenvwel@gmail.com>
2020-09-03 12:00:57 +0200

    Fix output WIP

--- Programma/main.cpp
+++ Programma/main.cpp
@@ -5,19 +5,92 @@
 #include "SortTests.h"
 #include "FindTests.h"
 #include <string>
+#include </usr/local/Cellar/jsoncpp/1.9.3/include/json/json.h>
 #include <fstream>
 
 #define TEST_COUNT 100000
 
+class results {
+public:
+    std::vector<clock_t> sortResults;
+    std::vector<clock_t> foldResults;
+    std::vector<clock_t> transformResults;
+    std::vector<clock_t> findResults;
+};
+
 int main() {
+    auto arrayResults = results();
+    auto vectorResults = results();
+    auto singlyListResults = results();
+    auto doublyListResults = results();
     for (int count = 0; count < TEST_COUNT; ++count) {
         auto subjects = TestSubjects();
-        auto sortResults = SortTests::test(subjects);
-        auto foldResults = FoldTests::test(subjects);
-        auto transformTests = TransformTests::test(subjects);
-        auto findTests = FindTests::test(subjects);
+        {
+            auto sortResults = SortTests::test(subjects);
+            arrayResults.sortResults.push_back(sortResults.arrayResult);
+            vectorResults.sortResults.push_back(sortResults.vectorResult);
+            singlyListResults.sortResults.push_back(sortResults.singlyListResult);
+            doublyListResults.sortResults.push_back(sortResults.doublyListResult);
+        }
+        {
+            auto foldResults = FoldTests::test(subjects);
+            arrayResults.foldResults.push_back(foldResults.arrayResult);
+            vectorResults.foldResults.push_back(foldResults.vectorResult);
+            singlyListResults.foldResults.push_back(foldResults.singlyListResult);
+            doublyListResults.foldResults.push_back(foldResults.doublyListResult);
+        }
+        {
+            auto transformResults = TransformTests::test(subjects);
+            arrayResults.transformResults.push_back(transformResults.arrayResult);
+            vectorResults.transformResults.push_back(transformResults.vectorResult);
+            singlyListResults.transformResults.push_back(transformResults.singlyListResult);
+            doublyListResults.transformResults.push_back(transformResults.doublyListResult);
+        }
+        {
+            auto findResults = FindTests::test(subjects);
+            arrayResults.findResults.push_back(findResults.arrayResult);
+            vectorResults.findResults.push_back(findResults.vectorResult);
+            singlyListResults.findResults.push_back(findResults.singlyListResult);
+            doublyListResults.findResults.push_back(findResults.doublyListResult);
+        }
     }
 
+    Json::Value array(Json::arrayValue);
+    Json::Value vector(Json::arrayValue);
+    Json::Value singlyList(Json::arrayValue);
+    Json::Value doublyList(Json::arrayValue);
 
+    for (int i = 0; i < TEST_COUNT; i++) {
+        array["sort"].append(Json::Value((u_int64_t)arrayResults.sortResults[i]));
+        array["fold"].append(Json::Value((u_int64_t)arrayResults.foldResults[i]));
+        array["transform"].append(Json::Value((u_int64_t)arrayResults.transformResults[i]));
+        array["find"].append(Json::Value((u_int64_t)arrayResults.findResults[i]));
+
+        vector["sort"].append(Json::Value((u_int64_t)vectorResults.sortResults[i]));
+        vector["fold"].append(Json::Value((u_int64_t)vectorResults.foldResults[i]));
+        vector["transform"].append(Json::Value((u_int64_t)vectorResults.transformResults[i]));
+        vector["find"].append(Json::Value((u_int64_t)vectorResults.findResults[i]));
+
+        singlyList["sort"].append(Json::Value((u_int64_t)singlyListResults.sortResults[i]));
+        singlyList["fold"].append(Json::Value((u_int64_t)singlyListResults.foldResults[i]));
+        singlyList["transform"].append(Json::Value((u_int64_t)singlyListResults.transformResults[i]));
+        singlyList["find"].append(Json::Value((u_int64_t)singlyListResults.findResults[i]));
+
+        doublyList["sort"].append(Json::Value((u_int64_t)doublyListResults.sortResults[i]));
+        doublyList["fold"].append(Json::Value((u_int64_t)doublyListResults.foldResults[i]));
+        doublyList["transform"].append(Json::Value((u_int64_t)doublyListResults.transformResults[i]));
+        doublyList["find"].append(Json::Value((u_int64_t)doublyListResults.findResults[i]));
+    }
+
+    Json::Value root;
+    root["array"] = array;
+    root["vector"] = vector;
+    root["singlyList"] =  singlyList;
+    root["doublyList"] = doublyList;
+
+    std::ofstream file("results.json");
+    auto writer = std::unique_ptr<Json::StreamWriter>(Json::StreamWriterBuilder().newStreamWriter());
+    writer->write(root, &file);
+    file << std::endl;
 }
 
