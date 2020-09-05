357fb45ad638707045e243cba9734a16c9b3a117
Koen <koenvwel@gmail.com>
2020-09-05 16:32:50 +0200

    It's never done

--- Programma/main.cpp
+++ Programma/main.cpp
@@ -6,20 +6,41 @@
 #include "FindTests.h"
 #include <string>
 #include <fstream>
+#include <utility>
 #include <json/json.h>
 
-#define TEST_COUNT 10000
+#define TEST_COUNT 100
+#define SAMPLE_COUNT 100
 
-class results {
-public:
+struct results {
     std::vector<clock_t> sortResults;
     std::vector<clock_t> foldResults;
     std::vector<clock_t> transformResults;
     std::vector<clock_t> findResults;
 };
 
-void saveData(results arrayResults, results vectorResults, results singlyListResults, results doublyListResults){
+struct means {
+    std::vector<double> sortMeans;
+    std::vector<double> foldMeans;
+    std::vector<double> transformMeans;
+    std::vector<double> findMeans;
+};
+
+std::vector<double> getAvg(results results) {
+    std::vector<double> res;
+    double size = TEST_COUNT;
+
+    res.push_back(((double)std::accumulate(results.sortResults.begin(), results.sortResults.end(), 0UL)) / size);
+    res.push_back(((double)std::accumulate(results.foldResults.begin(), results.foldResults.end(), 0UL))/size);
+    res.push_back(((double)std::accumulate(results.transformResults.begin(), results.transformResults.end(), 0UL))/size);
+    res.push_back(((double)std::accumulate(results.findResults.begin(), results.findResults.end(), 0UL))/size);
+
+    return res;
+}
+
+void saveData(means arrayMeans, means vectorMeans, means singlyListMeans, means doublyListMeans){
     std::cout << "## Saving data" << std::endl;
+    
     auto array = Json::Value();
     array["sort"] = Json::Value(Json::arrayValue);
     array["fold"] = Json::Value(Json::arrayValue);
@@ -45,25 +66,25 @@ void saveData(results arrayResults, results vectorResults, results singlyListRes
     doublyList["find"] = Json::Value(Json::arrayValue);
 
     for (int i = 0; i < TEST_COUNT; i++) {
-        array["sort"].append(Json::Value((u_int64_t)arrayResults.sortResults[i]));
-        array["fold"].append(Json::Value((u_int64_t)arrayResults.foldResults[i]));
-        array["transform"].append(Json::Value((u_int64_t)arrayResults.transformResults[i]));
-        array["find"].append(Json::Value((u_int64_t)arrayResults.findResults[i]));
+        array["sort"].append(Json::Value(arrayMeans.sortMeans[i]));
+        array["fold"].append(Json::Value(arrayMeans.foldMeans[i]));
+        array["transform"].append(Json::Value(arrayMeans.transformMeans[i]));
+        array["find"].append(Json::Value(arrayMeans.findMeans[i]));
 
-        vector["sort"].append(Json::Value((u_int64_t)vectorResults.sortResults[i]));
-        vector["fold"].append(Json::Value((u_int64_t)vectorResults.foldResults[i]));
-        vector["transform"].append(Json::Value((u_int64_t)vectorResults.transformResults[i]));
-        vector["find"].append(Json::Value((u_int64_t)vectorResults.findResults[i]));
+        vector["sort"].append(Json::Value(vectorMeans.sortMeans[i]));
+        vector["fold"].append(Json::Value(vectorMeans.foldMeans[i]));
+        vector["transform"].append(Json::Value(vectorMeans.transformMeans[i]));
+        vector["find"].append(Json::Value(vectorMeans.findMeans[i]));
 
-        singlyList["sort"].append(Json::Value((u_int64_t)singlyListResults.sortResults[i]));
-        singlyList["fold"].append(Json::Value((u_int64_t)singlyListResults.foldResults[i]));
-        singlyList["transform"].append(Json::Value((u_int64_t)singlyListResults.transformResults[i]));
-        singlyList["find"].append(Json::Value((u_int64_t)singlyListResults.findResults[i]));
+        singlyList["sort"].append(Json::Value(singlyListMeans.sortMeans[i]));
+        singlyList["fold"].append(Json::Value(singlyListMeans.foldMeans[i]));
+        singlyList["transform"].append(Json::Value(singlyListMeans.transformMeans[i]));
+        singlyList["find"].append(Json::Value(singlyListMeans.findMeans[i]));
 
-        doublyList["sort"].append(Json::Value((u_int64_t)doublyListResults.sortResults[i]));
-        doublyList["fold"].append(Json::Value((u_int64_t)doublyListResults.foldResults[i]));
-        doublyList["transform"].append(Json::Value((u_int64_t)doublyListResults.transformResults[i]));
-        doublyList["find"].append(Json::Value((u_int64_t)doublyListResults.findResults[i]));
+        doublyList["sort"].append(Json::Value(doublyListMeans.sortMeans[i]));
+        doublyList["fold"].append(Json::Value(doublyListMeans.foldMeans[i]));
+        doublyList["transform"].append(Json::Value(doublyListMeans.transformMeans[i]));
+        doublyList["find"].append(Json::Value(doublyListMeans.findMeans[i]));
     }
 
     Json::Value root;
@@ -78,49 +99,70 @@ void saveData(results arrayResults, results vectorResults, results singlyListRes
     file.close();
 }
 
+void addMeans(results results, means* means){
+    auto mean = getAvg(std::move(results));
+    means->sortMeans.push_back(mean[0]);
+    means->foldMeans.push_back(mean[1]);
+    means->transformMeans.push_back(mean[2]);
+    means->findMeans.push_back(mean[3]);
+}
+
 int main() {
-    auto arrayResults = results();
-    auto vectorResults = results();
-    auto singlyListResults = results();
-    auto doublyListResults = results();
-    for (int count = 0; count < TEST_COUNT; ++count) {
-        std::cout << "### Testcount:" << count << std::endl;
-        auto subjects = TestSubjects();
-        {
-            auto sortResults = SortTests::test(subjects);
-            std::cout << "## Adding sort test results" << std::endl;
-            arrayResults.sortResults.push_back(sortResults.arrayResult);
-            vectorResults.sortResults.push_back(sortResults.vectorResult);
-            singlyListResults.sortResults.push_back(sortResults.singlyListResult);
-            doublyListResults.sortResults.push_back(sortResults.doublyListResult);
+    means arrayMeans;
+    means vectorMeans;
+    means singlyListMeans;
+    means doublyListMeans;
+
+    for (int sampleN = 0; sampleN < SAMPLE_COUNT; ++sampleN) {
+        std::cout << "### Sample count:" << sampleN << std::endl;
+        results arrayResults;
+        results vectorResults;
+        results singlyListResults;
+        results doublyListResults;
+        for (int count = 0; count < TEST_COUNT; ++count) {
+            std::cout << "## Test count:" << count << std::endl;
+            auto subjects = TestSubjects();
+            {
+                auto sortResults = SortTests::test(subjects);
+                std::cout << "# Adding sort test results" << std::endl;
+                arrayResults.sortResults.push_back(sortResults.arrayResult);
+                vectorResults.sortResults.push_back(sortResults.vectorResult);
+                singlyListResults.sortResults.push_back(sortResults.singlyListResult);
+                doublyListResults.sortResults.push_back(sortResults.doublyListResult);
+            }
+            {
+                auto foldResults = FoldTests::test(subjects);
+                std::cout << "# Adding fold test results" << std::endl;
+                arrayResults.foldResults.push_back(foldResults.arrayResult);
+                vectorResults.foldResults.push_back(foldResults.vectorResult);
+                singlyListResults.foldResults.push_back(foldResults.singlyListResult);
+                doublyListResults.foldResults.push_back(foldResults.doublyListResult);
+            }
+            {
+                auto transformResults = TransformTests::test(subjects);
+                std::cout << "# Adding transform test results" << std::endl;
+                arrayResults.transformResults.push_back(transformResults.arrayResult);
+                vectorResults.transformResults.push_back(transformResults.vectorResult);
+                singlyListResults.transformResults.push_back(transformResults.singlyListResult);
+                doublyListResults.transformResults.push_back(transformResults.doublyListResult);
+            }
+            {
+                auto findResults = FindTests::test(subjects);
+                std::cout << "# Adding find test results" << std::endl;
+                arrayResults.findResults.push_back(findResults.arrayResult);
+                vectorResults.findResults.push_back(findResults.vectorResult);
+                singlyListResults.findResults.push_back(findResults.singlyListResult);
+                doublyListResults.findResults.push_back(findResults.doublyListResult);
+            }
         }
-        {
-            auto foldResults = FoldTests::test(subjects);
-            std::cout << "## Adding fold test results" << std::endl;
-            arrayResults.foldResults.push_back(foldResults.arrayResult);
-            vectorResults.foldResults.push_back(foldResults.vectorResult);
-            singlyListResults.foldResults.push_back(foldResults.singlyListResult);
-            doublyListResults.foldResults.push_back(foldResults.doublyListResult);
-        }
-        {
-            auto transformResults = TransformTests::test(subjects);
-            std::cout << "## Adding transform test results" << std::endl;
-            arrayResults.transformResults.push_back(transformResults.arrayResult);
-            vectorResults.transformResults.push_back(transformResults.vectorResult);
-            singlyListResults.transformResults.push_back(transformResults.singlyListResult);
-            doublyListResults.transformResults.push_back(transformResults.doublyListResult);
-        }
-        {
-            auto findResults = FindTests::test(subjects);
-            std::cout << "## Adding find test results" << std::endl;
-            arrayResults.findResults.push_back(findResults.arrayResult);
-            vectorResults.findResults.push_back(findResults.vectorResult);
-            singlyListResults.findResults.push_back(findResults.singlyListResult);
-            doublyListResults.findResults.push_back(findResults.doublyListResult);
-        }
+
+        addMeans(arrayResults, &arrayMeans);
+        addMeans(vectorResults, &vectorMeans);
+        addMeans(singlyListResults, &singlyListMeans);
+        addMeans(doublyListResults, &doublyListMeans);
     }
-    
-    saveData(arrayResults, vectorResults, singlyListResults, doublyListResults);
+
+    saveData(arrayMeans, vectorMeans, singlyListMeans, doublyListMeans);
 
     std::cout << "### Thank you, come again!" << std::endl;
 }
